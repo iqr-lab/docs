308633459e93e8114d226de61c0b272a7dbcb1ca
Neil Song <song.neil.song@gmail.com>
2025-01-30 15:10:06 -0500

    feat: iqr-vision docs

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -46,6 +46,7 @@ export default defineUserConfig({
           "intel-realsense",
           "kalibr",
           "pointcloud-stitching",
+          "iqr-vision",
         ],
       },
       {
---
+++ computer-vision/iqr-vision.md
@@ -0,0 +1,3 @@
+# Placeholder for iqr-vision.md
+
+Replaced on static site build with contents from [iqr-vision repo README.md](https://github.com/iqr-lab/iqr-vision/blob/main/README.md)
